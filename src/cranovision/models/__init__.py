27fd758f818775eb1f8fb0b050753948ed124078
DPHeshanRanasinghe <ranasinghedph.23@uom.lk>
2026-04-18 04:56:38 +0530

    fix: remove deprecated img_size arg from SwinUNETR (MONAI 1.5)

--- src/cranovision/models/__init__.py
+++ src/cranovision/models/__init__.py
@@ -2,8 +2,14 @@
 CranioVision model architectures.
 """
 from .attention_unet import build_attention_unet, count_parameters
+from .swin_unetr import build_swin_unetr, load_pretrained_weights
 
 __all__ = [
+    # Attention U-Net
     "build_attention_unet",
+    # SwinUNETR
+    "build_swin_unetr",
+    "load_pretrained_weights",
+    # Shared utility
     "count_parameters",
 ]
