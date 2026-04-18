203afaea3c8d70159686d5032387930aa8088bb9
DPHeshanRanasinghe <ranasinghedph.23@uom.lk>
2026-04-18 11:12:43 +0530

    feat: MC Dropout uncertainty quantification

--- src/cranovision/inference/__init__.py
+++ src/cranovision/inference/__init__.py
@@ -8,11 +8,23 @@ from .predict import (
     make_inferer,
     compute_region_volumes,
 )
+from .mc_dropout import (
+    mc_dropout_predict,
+    summarize_confidence,
+    enable_dropout,
+    count_dropout_layers,
+)
 
 __all__ = [
+    # predict
     "load_model",
     "preprocess_case",
     "predict_case",
     "make_inferer",
     "compute_region_volumes",
+    # mc_dropout
+    "mc_dropout_predict",
+    "summarize_confidence",
+    "enable_dropout",
+    "count_dropout_layers",
 ]
